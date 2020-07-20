c60d1d34448491aae2705bed281daf5b195c5261
sardongui <saradg1995@gmail.com>
2020-07-20 18:37:21 +0200

    Creacion entity

---
+++ src/main/java/acme/features/anonymous/notices/AnonymousNoticeListService.java
@@ -0,0 +1,61 @@
+
+package acme.features.anonymous.notices;
+
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.notices.Notice;
+import acme.framework.components.Model;
+import acme.framework.components.Request;
+import acme.framework.entities.Anonymous;
+import acme.framework.services.AbstractListService;
+
+@Service
+public class AnonymousNoticeListService implements AbstractListService<Anonymous, Notice> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	AnonymousNoticeRepository repository;
+
+	@Override
+	public boolean authorise(final Request<Notice> request) {
+		assert request != null;
+
+		return true;
+	}
+
+	@Override
+	public Collection<Notice> findMany(final Request<Notice> request) {
+		assert request != null;
+
+		Calendar c;
+		c = new GregorianCalendar();
+		c.add(Calendar.MONTH, -1);
+		Date d = c.getTime();
+
+		Collection<Notice> result;
+		result = this.repository.findMany(d);
+
+		return result;
+	}
+
+	@Override
+	public void unbind(final Request<Notice> request, final Notice entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "headerPicture", "title", "creationMoment", "deadline", "body", "links");
+		
+	}
+
+	
+
+
+}
---
+++ src/main/java/acme/features/anonymous/notices/AnonymousNoticeRepository.java
@@ -0,0 +1,19 @@
+
+package acme.features.anonymous.notices;
+
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.notices.Notice;
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface AnonymousNoticeRepository extends AbstractRepository {
+	
+	@Query("select n from Notice n where n.id = ?1")
+	Notice findOneById(int id);
+	
+	@Query("select n from Notice n where n.creationDate = ?1")
+	Notice findMany(int id);
+}
