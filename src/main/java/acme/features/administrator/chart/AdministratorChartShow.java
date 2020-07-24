0d011b41aecd4a6e3ab9403bf59233fe89fa5c4b
sardongui <saradg1995@gmail.com>
2020-07-24 17:32:02 +0200

    Chart creado

---
+++ src/main/java/acme/features/administrator/chart/AdministratorChartShow.java
@@ -0,0 +1,57 @@
+
+package acme.features.administrator.chart;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.forms.Chart;
+import acme.framework.components.Model;
+import acme.framework.components.Request;
+import acme.framework.entities.Administrator;
+import acme.framework.services.AbstractShowService;
+
+@Service
+public class AdministratorChartShow implements AbstractShowService<Administrator, Chart> {
+
+	@Autowired
+	AdministratorChartRepository repository;
+
+
+	@Override
+	public boolean authorise(final Request<Chart> request) {
+		assert request != null;
+		return true;
+	}
+
+	@Override
+	public void unbind(final Request<Chart> request, final Chart entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "numberOfTechonologiesGroupedBySector", "numberOfToolsGroupedBySector");
+		request.unbind(entity, model, "ratioOfTechnologiesGroupedByStatus", "ratioOfToolsGroupedByStatus");
+		
+
+	}
+
+	@Override
+	public Chart findOne(final Request<Chart> request) {
+		assert request != null;
+
+
+		Chart d = new Chart();
+		Object[] companiesBySector = this.repository.findTechnologiesSector();
+		d.setNumberOfTechonologiesGroupedBySector(companiesBySector);
+		Object[] investorsBySector = this.repository.findToolsSector();
+		d.setNumberOfToolsGroupedBySector(investorsBySector);
+		Object[] technologiessByStatus = this.repository.findTechnologiesStatus();
+		d.setRatioOfTechnologiesGroupedByStatus(technologiessByStatus);
+		Object[] toolsByStatus = this.repository.findToolsStatus();
+		d.setRatioOfToolsGroupedByStatus(toolsByStatus);
+		
+		return d;
+	}
+
+
+}
