309725e0902ab7766ba158395936737d0e9729c8
Jose Carlos <josgongar12@alum.us.es>
2020-07-26 21:19:01 +0200

    Gonzalez Bulletin finished

--- src/main/java/acme/entities/bulletins/GonzalezBulletin.java
+++ src/main/java/acme/entities/bulletins/GonzalezBulletin.java
@@ -9,8 +9,6 @@ import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
 
-import org.hibernate.validator.constraints.URL;
-
 import acme.framework.entities.DomainEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,11 +21,10 @@ public class GonzalezBulletin extends DomainEntity {
 	private static final long	serialVersionUID	= 1L;
 
 	@NotBlank
-	@URL
-	private String				web;
+	private String				movie;
 
 	@NotBlank
-	private String				description;
+	private String				director;
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@Past
--- src/main/java/acme/features/anonymous/gonzalezBulletin/AnonymousGonzalezBulletinListService.java
+++ src/main/java/acme/features/anonymous/gonzalezBulletin/AnonymousGonzalezBulletinListService.java
@@ -34,7 +34,7 @@ public class AnonymousGonzalezBulletinListService implements AbstractListService
 		assert entity != null;
 		assert model != null;
 
-		request.unbind(entity, model, "web", "description", "moment");
+		request.unbind(entity, model, "movie", "director", "moment");
 
 	}
 
